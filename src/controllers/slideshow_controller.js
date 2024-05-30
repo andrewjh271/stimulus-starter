8cf03af87252913c07a8271da9d82427ec888e32
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2024-05-29 18:28:23 -0700

    Slideshow / managing state

---
+++ src/controllers/slideshow_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "slide" ];
+  static values = { index: Number }
+
+  next() {
+    this.indexValue++;
+    if(this.indexValue > 3) this.indexValue =  0;
+  }
+
+  previous() {
+    this.indexValue--;
+    if(this.indexValue < 0) this.indexValue = 3;
+  }
+
+  indexValueChanged() {
+    this.showCurrentSlide();
+  }
+
+  showCurrentSlide() {
+    this.slideTargets.forEach((element, index) => {
+      element.hidden = (index !== this.indexValue)
+    })
+  }
+}
